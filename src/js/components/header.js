8f1cbd6845956b9f44b9cdb31483271f1cf02acd
qwertycamedy <qwertycamedy@gmail.com>
2021-09-06 12:20:58 +0600

    изменил фотографии

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -3,7 +3,7 @@ const headerLink = document.querySelectorAll('.header__list-link');
 for (var i = headerLink.length - 1; i >= 0; i--) {
   if (headerLink[i].href == document.URL) {
     headerLink[i].classList.add('active');
-  }
+  } 
 };
 
 let listButton = document.querySelector('.header__list-button'),
